51321aa9338d716b6f0c4ce9d9137edebf7bca68
sumith <sumith95023@gmail.com>
2023-11-08 14:57:24 +0530

    updated

--- information/lawyersinfo.js
+++ information/lawyersinfo.js
@@ -10,14 +10,15 @@ export default function Lawyers() {
                         <Text style={{fontSize:15,textAlign:'center',}}>Address: 89,shahjahan road,New Delhi</Text>
                         <Text style={{fontSize:15,textAlign:'center',}}>Date of designation as senior advocate: 26-3-1966</Text>
                         <Text style={{fontSize:15,textAlign:'center',}}>Remarks: Retired Judge,Bombay High Court</Text>
-                        <View style={{flex:1,margin:5,textAlign:'center',width:100}} >< Button title= "contact" onPress={() => Alert.alert('contact now!!')}/></View>
+                        <View style={{flexDirection:'row',justifyContent: 'center',alignItems:'center'}} >
+                            < Button title= "contact" onPress={() => Alert.alert('contact now!!')}/></View>
                     </View>
                     <View style={styles.bg}>
                         <Text style={{fontSize:28,color:'black',textAlign:'center',}} >Mr. Hari Ramchandra Gokhle</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Address: Rocky Hill Flats,N.Dabholkar Road,Bombay-6</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Date of designation as senior advocate: 23-7-1966</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Remarks: Advocate</Text>
-                        <View style={{margin:5,textAlign:'center',width:100}}><Button title= "contact"/></View>
+                        <View style={{flexDirection:'row',justifyContent: 'center',alignItems:'center'}}><Button title= "contact"/></View>
                     </View>
 
                     <View style={styles.bg}>
@@ -25,21 +26,21 @@ export default function Lawyers() {
                         <Text style={{fontSize:15,textAlign:'center'}}>Address: c-53,south Extn.Part-1,New Delhi</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Date of designation as senior advocate: 3-10-1966</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Remarks: Advocate</Text>
-                        <View style={{margin:5,textAlign:'center',width:100}}><Button title= "contact"/></View>
+                        <View style={{flexDirection:'row',justifyContent: 'center',alignItems:'center'}}><Button title= "contact"/></View>
                     </View>
                     <View style={styles.bg}>
                         <Text style={{fontSize:28,color:'black',textAlign:'center',}}>Dr.V.A.Sayed Muhammed</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Address: Address not available</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Date of designation as senior advocate: 7-11-1966</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Remarks: Advocate General</Text>
-                        <View style={{margin:5,textAlign:'center',width:100}}><Button title= "contact"/></View>
+                        <View style={{flexDirection:'row',justifyContent: 'center',alignItems:'center'}}><Button title= "contact"/></View>
                     </View>
                     <View style={styles.bg}>
                         <Text style={{fontSize:28,color:'black',textAlign:'center',}}>Mr.N.D.Khar Khanis</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Address: 122,Golf Links,N.D.</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Date of designation as senior advocate: 7-10-1967</Text>
                         <Text style={{fontSize:15,textAlign:'center'}}>Remarks: Advocate</Text>
-                        <View style={{margin:5,textAlign:'center',width:100}}><Button title= "contact"/></View>
+                        <View style={{flexDirection:'row',justifyContent: 'center',alignItems:'center'}}><Button title= "contact"/></View>
                     </View>
                 </View>
         </ScrollView>
@@ -55,6 +56,8 @@ const styles= StyleSheet.create({
         color:'black',
         padding:10,
         margin:10,
+    
+
     }
 
 
