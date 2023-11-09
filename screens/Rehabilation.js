b64ce984407c2dcaf0adbfbf86fcc1d305e2b1a7
sumith <sumith95023@gmail.com>
2023-11-09 14:14:55 +0530

    see youtube video to install packages

--- screens/Rehabilation.js
+++ screens/Rehabilation.js
@@ -1,18 +1,44 @@
 import React from "react";
-import { StyleSheet,View,Text } from "react-native";
+import { StyleSheet,View,Text ,ScrollView} from "react-native";
+import YoutubePlayer from "react-native-youtube-iframe";
 
 export default function Rehabilation() {
     return (
-        <View style={styles.texts}>
-            <Text> Welcome to Rehabilation Center...!!! </Text>
-            
-        </View>
+        <ScrollView>
+            <View style={styles.youtube}> 
+            <YoutubePlayer 
+                height={300}
+                width={300}
+                play={false}
+                videoId={'sNaHN4tZmRk'}
+            />
+           <View style={styles.youtube} >
+            <YoutubePlayer 
+            height={300}
+            width={300}
+            play={false}
+            videoId={'e037nbVAwm8'}
+            />                    
+            </View>
+            <View style={styles.youtube}>
+            <YoutubePlayer 
+            height={300}
+            width={300}
+            play={true}
+            videoId={'e037nbVAwm8'}
+            />                    
+            </View>
+       </View> 
+        </ScrollView>
+       
+     
     ) 
 }
 const styles=StyleSheet.create({
-    texts:{
-        flex:1,
+    youtube:{
+        flexDirection:'column',
         alignItems:"center",
         justifyContent:"center",
+        margin:30,
       }
 })
