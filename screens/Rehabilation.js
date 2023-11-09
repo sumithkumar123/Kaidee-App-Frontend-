b3fb137a6f6649ec1d5dd5d98da74ae1e5303313
sumith <sumith95023@gmail.com>
2023-11-09 14:36:21 +0530

    updated

--- screens/Rehabilation.js
+++ screens/Rehabilation.js
@@ -6,24 +6,30 @@ export default function Rehabilation() {
     return (
         <ScrollView>
             <View style={styles.youtube}> 
+            <Text style={styles.texts}>Carpentry</Text>
             <YoutubePlayer 
                 height={300}
-                width={300}
+                width={400}
                 play={false}
                 videoId={'sNaHN4tZmRk'}
             />
+            <Text style={styles.texts}>Welding</Text>
            <View style={styles.youtube} >
             <YoutubePlayer 
             height={300}
-            width={300}
+            width={400}
             play={false}
             videoId={'e037nbVAwm8'}
             />                    
             </View>
+            <View>
+            <Text style={styles.texts}>Tailoring</Text>
+            </View>
+            
             <View style={styles.youtube}>
             <YoutubePlayer 
             height={300}
-            width={300}
+            width={400}
             play={true}
             videoId={'e037nbVAwm8'}
             />                    
@@ -39,6 +45,12 @@ const styles=StyleSheet.create({
         flexDirection:'column',
         alignItems:"center",
         justifyContent:"center",
-        margin:30,
-      }
+        margin:20,   
+      },
+    texts: {
+        padding:10,
+        height:30,
+        margin:5,
+        fontSize:24,
+    }
 })
--- screens/mainScreen.js
+++ screens/mainScreen.js
@@ -32,7 +32,7 @@ export default function MainScreen({navigation}) {
         }, 
         headerTintColor: '#fff',
        
-        headerTitleStyle: {
+        headerTitleStyle: { 
           fontWeight: 'bold',
         },  
 
