cd8387e6ab42547b6206e962273b4fb3527fd942
sumith <sumith95023@gmail.com>
2023-11-11 01:44:20 +0530

    updated rehabilation and header

--- screens/Rehabilation.js
+++ screens/Rehabilation.js
@@ -3,13 +3,24 @@ import { StyleSheet,View,Text ,ScrollView} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import VocationalStackScreen from "../routes/vocationalstackscreen";
+import EducationalStackScreen from "../routes/educationalStackScreen";
+import RehabilationHome from "./rehabilationHome";
+import MentalHealthStackScreen from "../routes/mentaHealthStackScreen";
+
 const RehabilationStack=createNativeStackNavigator(); 
 
 export default function Rehabilation() {
     return (
         <RehabilationStack.Navigator 
-        screenOptions={{ headerShown: false }}>
+        screenOptions={{ headerShown: false }} 
+        initialRouteName="RehabilationHome"> 
+
+         <RehabilationStack.Screen name="RehabilationHome" component={RehabilationHome} />
           <RehabilationStack.Screen name="VocationalStackScreen" component={VocationalStackScreen} />
+          <RehabilationStack.Screen name="EducationalStackScreen" component={EducationalStackScreen} />
+          <RehabilationStack.Screen name="MentalHealthStackScreen" component={MentalHealthStackScreen} />
+
+
           {/* <Stack.Screen name="DrawerScreen" component={DrawerScreen} /> */}
   
         </RehabilationStack.Navigator>
