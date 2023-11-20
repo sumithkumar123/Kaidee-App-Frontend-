990e0e5e7157fa8e7f8d19b054f2079811850d15
sivani-1 <147125105+sivani-1@users.noreply.github.com>
2023-11-20 16:20:02 +0530

    jail1

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,56 +1,109 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button , StyleSheet } from 'react-native';
-
+import { View, Text, TextInput, Button, StyleSheet, ImageBackground } from 'react-native';
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.hea}>Login Page</Text>
-      <TextInput style={styles.inp}
-        placeholder="Email"
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput style={styles.inp}
-        placeholder="Password"
-        secureTextEntry
-        onChangeText={(text) => setPassword(text)}
-      />
-      <View style={styles.butt}>
-        <Button title="Login" onPress={() => navigation.navigate('DrawerScreen')} />
+    <ImageBackground
+      source={require('../assets/jail1.png')} 
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>UNDERTRIAL PRISONERS</Text>
+        <View style={styles.content}>
+          <Text style={styles.hea}></Text>
+          <TextInput
+            style={styles.inp}
+            placeholder="Email"
+            onChangeText={(text) => setEmail(text)}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <TextInput
+            style={styles.inp}
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+          />
+          <View style={styles.butt}>
+            <Button title="Login" onPress={() => navigation.navigate('DrawerScreen')} />
+          </View>
+          <Text style={styles.forgotPassword}>Forgot Password?</Text>
+          <View style={styles.signupContainer}>
+            <Text style={styles.bothea}>Don't have an account?</Text>
+            <Text style={styles.signupText} onPress={() => navigation.navigate('SignUpScreen')}>
+              Sign up here
+            </Text>
+          </View>
+        </View>
       </View>
-      <Text style={styles.bothea} onPress={() => navigation.navigate('SignUpScreen')}>Don't have an account? Sign up here</Text>
-    </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', 
+  },
   container: {
-    padding: 50,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)', 
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 50, 
+    color: '#fff',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   hea: {
-    padding: 30,
-     fontSize: 30,
-     fontFamily: "Roboto",
+    fontSize: 30,
+    fontFamily: 'Roboto',
+    marginBottom: 30,
+    color: '#fff',
   },
   inp: {
+    borderRadius: 5,
     borderWidth: 2,
     height: 40,
-    width: 200,
-    padding: 5,
-    margin: 30,
+    width: 300,
+    padding: 10,
+    marginVertical: 10,
+    borderColor: '#ccc',
+    backgroundColor: '#fff',
+    color: '#333', 
   },
   butt: {
+    width: 200,
+    marginVertical: 20,
+  },
+  forgotPassword: {
+    color: '#3498db',
+    fontSize: 16,
+    marginBottom: 20,
+    color: '#fff',
+  },
+  signupContainer: {
     flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems:"center"
+    alignItems: 'center',
   },
   bothea: {
-    fontSize: 20,
-    padding: 30,
+    fontSize: 16,
+    marginRight: 5,
+    color: '#fff',
+  },
+  signupText: {
+    fontSize: 16,
+    color: '#3498db',
   },
 });
 
